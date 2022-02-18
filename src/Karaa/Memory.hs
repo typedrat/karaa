b71791d3c867790da89f39102f56d8352083cc13
Alexis Williams <alexis@typedr.at>
2022-02-17 17:49:31 -0800

    Start from scratch.

--- src/Karaa/Memory.hs
+++
@@ -1,6 +0,0 @@
-module Karaa.Memory ( readRAM, writeRAM, module I, module T ) where
-
-import Karaa.Memory.Access
-    ( readRAM, writeRAM )
-import Karaa.Memory.Initialize as I
-import Karaa.Memory.Types      as T
--- src/Karaa/Memory/Cartridge.hs
+++
@@ -1,7 +0,0 @@
-module Karaa.Memory.Cartridge ( readCartridge ) where
-
-import Karaa.Memory.Types 
-    ( MBC(..) )
-
-readCartridge :: FilePath -> Maybe FilePath -> IO MBC
-readCartridge _ _ = fail "readCartridge is unimplemented."
